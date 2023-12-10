1265405151a12ff06a52d4f95e83e22e4968cfa0
Mamad Jadali <jadalimamad@gmail.com>
2023-12-10 04:01:43 +0330

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://mamadjadali.github.io',
 	integrations: [
 		starlight({
 			title: 'REP',
